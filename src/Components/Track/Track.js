c897b7623b39ae3a051df95529a7d77413c57772
Luke <luke.michalek@gmail.com>
2023-01-04 20:29:44 +0000

    Pass Down Search Result and Render Result List AND Pass down Playlist to TrackList

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Track.css";
 
-class Track extends React.Component {
+export class Track extends React.Component {
 
-    renderAction(isRemoval){
-        if (isRemoval){
-            <button className = "Track-action">-</button>
+    // Step 27 -> Button will show + if isRemoval is false and - if is Removal is True -> Note that I do not have to use isRemoval === true as JSX expects non False value
+    renderAction(){
+        if (this.props.isRemoval){
+            return <button className = "Track-action">-</button>
         }
         else{
-            <button className = "Track-action"></button>
+            return <button className = "Track-action"></button>
 
         }
     }
@@ -18,11 +19,11 @@ class Track extends React.Component {
     return (
       <div className="Track">
         <div className="Track-information">
-          {/* <h3><!-- track name will go here --></h3> */}
-          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
         <button className="Track-action">
-          {/*<!-- + or - will go here -->*/}
+          {/* {this.renderAction()} */}
         </button>
       </div>
     );
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./TrackList.css";
+import { Track } from "../Track/Track";
 
 export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
         {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {/* Basically what I will do is to take an array of all fetched data passed all the way down from App(that will receive data from API as an array of objects(having name, artist, album and id from becasue Spotify API stores them in this way) and are in form of state as this is the main component) which are passed to SearchResult Component(in form of props as this is child of main component app -> Also I need to check if data are passed correctly in DeV Tools -> Raeact -> Components) and these are sent to TrackList from SearchReslut in form of props. In Tracklist the array of objects will be transformed into individual Track compomenets using map() method. So basically each track will get its own component called Track => Creates Track componenet with atribute called track and each track id will be stored as key     */}
+
+        {
+         this.props.tracks.map((track) => {
+          return < Track track={track} key={track.id} />
+        })
+        }
       </div>
-    );
+    )
   }
 }
